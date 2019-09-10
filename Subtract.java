f498fac730b683050b67873305e4c6b0a3bfac58
Jmiller1294 <54234620+Jmiller1294@users.noreply.github.com>
2019-09-10 16:53:42 -0400

    Add files via upload

---
+++ Subtract.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Subtract {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		//Introduce the program
+		System.out.println("Welcome, This program displays the subtraction of five numbers");
+		//Ask user for input
+		System.out.println("Enter five numbers");
+		
+		//Wait for user input and store
+		int num1 = input.nextInt();
+		int num2 = input.nextInt();
+		int num3 = input.nextInt();
+		int num4 = input.nextInt();
+		int num5 = input.nextInt();
+		
+		
+		//Display result
+		System.out.println(num1 + "-" + num2 + "-" + num3 + "-" + num4 + "-" + num5);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
