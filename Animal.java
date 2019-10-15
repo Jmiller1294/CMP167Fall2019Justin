9a9456a6993d131bcf666295a7a6c9a77a696ca0
Justin Miller <54234620+Jmiller1294@users.noreply.github.com>
2019-10-15 16:28:46 -0400

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,22 @@
+
+public class Animal
+{
+	    public int numLegs;
+		private int age;
+		
+		public void bark()
+		{
+			System.out.println("Woof!");
+			
+        }
+			
+			
+		public void run()
+		{
+			
+			
+		}
+			
+		
+}
+
